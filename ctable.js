7c951b429534b1bdbdc37c3ca17b2e55abaa1772
Innocent Unachukwu <innocent.unachukwu@gmail.com>
2022-04-21 13:43:27 +0100

    Heroku commit

--- ctable.js
+++ ctable.js
@@ -15,47 +15,24 @@ const app = express();
 
 
 
-/*
   dbconnection.connect((err) => {
-      if(!err){
-          console.log('Connection ok');
+    if (err){
+       throw err;
+    }else{   
+      console.log("Connected!");
 
-          let sql = `CREATE TABLE customers (
-            ID int(11) NOT NULL AUTO,
-            NAME varchar(50) NOT NULL,
-            EMAIL varchar(50) NOT NULL,
-            PASSWORD varchar(50) NOT NULL,
-            BALANCE int(11) NOT NULL
-          )`;
-    
-        dbconnection.query(sql, (err, results) => {
-            if(err){
-                throw err;
-            }else{
-                console.log(results);
-                res.send("Table created");
-            }
-      }
-      else{
-          throw err;
-      }
-  });
-*/
- 
+      //var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+      let sql = 'CREATE TABLE customers' (
+          ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+          NAME VARCHAR(50) NOT NULL,
+          EMAIL VARCHAR(50) NOT NULL,
+          PASSWORD VARCHAR(50) NOT NULL,
+          BALANCE INT(11) NOT NULL,
+        );
 
-  dbconnection.connect((err) => {
-    if (err) throw err;
-    console.log("Connected!");
-    //var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
-    let sql = `CREATE TABLE customers (
-        ID INT(11) NOT NULL AUTO INCREMENT PRIMARY KEY,
-        NAME VARCHAR(50) NOT NULL,
-        EMAIL VARCHAR(50) NOT NULL,
-        PASSWORD VARCHAR(50) NOT NULL,
-        BALANCE INT(11) NOT NULL
-      )`;
-      dbconnection.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log("Table created");
-    });
+        dbconnection.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Table created");
+      });
+    }  
   });
--- index.js
+++ index.js
@@ -6,12 +6,19 @@ const app = express();
 //Create DB connection
  var dbconnection = mysql.createConnection({
 
+    //Using localhost DB
 //     host     : 'localhost',
 //     user     : 'root',
 //     password : '',
 //     database : 'mynodesqldb'
+
+//Using online msqlDB
+//     host     : 'https://pdb11.runhosting.com',
+//     user     : '2107412_nodemysql',
+//     password : 'nodeMYsqlpass21',
+//     database : '2107412_nodemysql'
     
-    
+    //Mysql ClearDB on Heroku
     host     : 'eu-cdbr-west-02.cleardb.net',
     user     : 'bbeed19b5da297',
     password : 'acc38556',
@@ -27,28 +34,6 @@ const app = express();
           throw err;
       }
   });
-
-
-   //Create customers table
-//    app.get('/createtable', (req,res) => {
-    // dbconnection.connect((err) => {
-    //     if (err) throw err;
-    //     console.log("Connected!");
-    //     //var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
-//         let sql = "CREATE TABLE customers (
-//             ID INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-//             NAME VARCHAR(50) NOT NULL,
-//             EMAIL VARCHAR(50) NOT NULL,
-//             PASSWORD VARCHAR(50) NOT NULL,
-//             BALANCE INT(11) NOT NULL
-//           )";
-//           dbconnection.query(sql, function (err, result) {
-//           if (err) throw err;
-//           console.log("Table created");
-//           res.send("table created...")
-//         });
-//       });
-// })
 
 
     //Create Home page
